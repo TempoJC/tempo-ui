0603771438b57e98735264128f45d1057033a596
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2025-07-11 11:12:28 +0200

    feat: create loader component

---
+++ src/components/Loaders/Loader/constants.ts
@@ -0,0 +1,9 @@
+export const LOADER_CIRCULAR = "circular";
+export const LOADER_BAR = "bar";
+
+export const LOADER_DARK = "dark";
+export const LOADER_LIGHT = "light";
+
+export const LOADER_CIRCULAR_SMALL = "small";
+export const LOADER_CIRCULAR_MEDIUM = "medium";
+export const LOADER_CIRCULAR_LARGE = "large";
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from "./Button";
 export * from "./Card";
 export * from "./Icons/Icon";
+export * from "./Loaders/Loader";
 // Agrega más exports a medida que crees más componentes
