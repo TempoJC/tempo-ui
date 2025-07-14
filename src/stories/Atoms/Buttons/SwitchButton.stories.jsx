2da227387ef824408527713c70017a1765dd41b7
Jose Luis Camargo Calizaya <josel.cc.dam@gmail.com>
2025-07-14 14:39:31 +0200

    feat: add buttons components

---
+++ src/stories/Atoms/Buttons/SwitchButton.stories.jsx
@@ -0,0 +1,64 @@
+import { SwitchButton } from "@/components";
+import { fn } from "storybook/test";
+
+const meta = {
+  title: "Example/Atoms/Buttons/SwitchButton",
+  component: SwitchButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    selected: { control: "boolean" },
+    disabled: { control: "boolean" },
+    focused: { control: "boolean" },
+    color: {
+      control: { type: "select" },
+      options: ["light", "dark"],
+    },
+    icon: { control: false },
+    onClick: { action: "clicked" },
+    onMouseDown: { action: "mouse down" },
+    onMouseUp: { action: "mouse up" },
+    onMouseLeave: { action: "mouse leave" },
+    onTouchStart: { action: "touch start" },
+    onTouchEnd: { action: "touch end" },
+  },
+  args: {
+    onClick: fn(),
+    onMouseDown: fn(),
+    onMouseUp: fn(),
+    onMouseLeave: fn(),
+    onTouchStart: fn(),
+    onTouchEnd: fn(),
+  },
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    icon: "☀️",
+  },
+};
+
+export const Selected = {
+  args: {
+    icon: "🌙",
+    selected: true,
+  },
+};
+
+export const Disabled = {
+  args: {
+    icon: "🚫",
+    disabled: true,
+  },
+};
+
+export const Dark = {
+  args: {
+    icon: "💡",
+    color: "dark",
+  },
+};
